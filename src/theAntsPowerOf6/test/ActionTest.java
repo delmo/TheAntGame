dfcc4c34ab52cb9fa79927b91329b7acb9412ac6
ryan <rtgd20@sussex.ac.uk>
2013-04-15 23:38:20 +0100

    Create testing system.

---
+++ src/theAntsPowerOf6/test/ActionTest.java
@@ -0,0 +1,138 @@
+/**
+ * 
+ */
+package theAntsPowerOf6.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import theAntsPowerOf6.model.Action;
+
+/**
+ * @author Rhayan
+ *
+ */
+public class ActionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#Action(java.lang.String)}.
+	 */
+	@Test
+	public void testAction() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getSt1()}.
+	 */
+	@Test
+	public void testGetSt1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getSt2()}.
+	 */
+	@Test
+	public void testGetSt2() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getMark()}.
+	 */
+	@Test
+	public void testGetMark() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getNumber()}.
+	 */
+	@Test
+	public void testGetNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getInput()}.
+	 */
+	@Test
+	public void testGetInput() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getInstruction()}.
+	 */
+	@Test
+	public void testGetInstruction() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getDir()}.
+	 */
+	@Test
+	public void testGetDir() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getCon()}.
+	 */
+	@Test
+	public void testGetCon() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#getTurn()}.
+	 */
+	@Test
+	public void testGetTurn() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Action#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/theAntsPowerOf6/test/AntTest.java
@@ -0,0 +1,180 @@
+/**
+ * 
+ */
+package theAntsPowerOf6.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import theAntsPowerOf6.model.Ant;
+import theAntsPowerOf6.model.AntColor;
+import theAntsPowerOf6.model.Position;
+
+/**
+ * @author Rhayan
+ *
+ */
+public class AntTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#Ant(int, theAntsPowerOf6.model.AntColor, theAntsPowerOf6.model.Position)}.
+	 */
+	@Test
+	public void testAnt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#getState()}.
+	 */
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#setState(int)}.
+	 */
+	@Test
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#isColour()}.
+	 */
+	@Test
+	public void testIsColour() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#getResting()}.
+	 */
+	@Test
+	public void testGetResting() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#setResting()}.
+	 */
+	@Test
+	public void testSetResting() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#decResting()}.
+	 */
+	@Test
+	public void testDecResting() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#getDirection()}.
+	 */
+	@Test
+	public void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#setDirection(int)}.
+	 */
+	@Test
+	public void testSetDirection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#hasfood()}.
+	 */
+	@Test
+	public void testHasfood() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#setHasfood(boolean)}.
+	 */
+	@Test
+	public void testSetHasfood() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#step(int)}.
+	 */
+	@Test
+	public void testStep() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#move(theAntsPowerOf6.model.Position)}.
+	 */
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#getPosition()}.
+	 */
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link theAntsPowerOf6.model.Ant#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
